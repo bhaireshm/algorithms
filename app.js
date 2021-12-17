cab37ca61a43c30a5b9cd011f77e5b42b2b8877f
Bhairesh M <bhairesh@arithaconsulting.com>
2021-12-17 23:50:00 +0530

    Update: CLI-UI updated.

--- app.js
+++ app.js
@@ -26,33 +26,11 @@ if (Object(prompt.override).hasOwnProperty("fileNumber")) {
   prompt.stop();
 } else {
   // * Log all files and folders
-  console.log("\n");
-  Object.keys(folderStructure).forEach((key, i) => {
-    console.log("Folder: ", key);
-    for (let j = 0; j < folderStructure[key].length; j++) {
-      const file = folderStructure[key][j];
-      console.log("         ", file);
-    }
-    console.log("\n");
-  });
-
-  console.log("Select a file to execute");
+  logFiles();
   prompt.start();
 }
 
-prompt.get(["fileNumber"], function (err, result) {
-  if (err) console.error(err);
-
-  console.log("Input:", result);
-  const fileFullPath = getFile(result.fileNumber);
-
-  exec(`node ${fileFullPath}`, function (err, stdout, stderr) {
-    if (err) console.error(err);
-    if (stderr) console.error(stderr);
-    if (stdout) console.log(stdout);
-    process.exit(0);
-  });
-});
+startPrompt();
 
 // * ------------------------------------------------------------------------- * //
 // * Private functions
@@ -66,23 +44,82 @@ function getPath(fp) {
   return path.join(__dirname, fp);
 }
 
+function logFiles() {
+  console.log("\n");
+  Object.keys(folderStructure).forEach((key, i) => {
+    helper.log(" " + key.split("_").slice(1).join(" ").toUpperCase());
+    for (let j = 0; j < folderStructure[key].length; j++) {
+      const file = folderStructure[key][j];
+      console.log(" ".repeat(3), file);
+    }
+    console.log("\n");
+  });
+
+  console.log("Select a file to execute");
+}
+
 function getFile(fileNumber) {
   const indexes = String(fileNumber).split(".");
   const folderName = Object.keys(folderStructure)[indexes[0] - 1];
   if (!folderName) {
     helper.error("Folder not found!!");
-    process.exit(1);
+    return false;
   }
   const fileName = Object.values(folderStructure)[indexes[0] - 1][indexes[1] - 1];
   if (!fileName) {
     helper.error("File not found!!");
-    process.exit(1);
+    return false;
   }
   const actualFileName = fileName.replace(`${fileNumber}_`, "");
   const fileFullPath = getPath(path.join(folderName, actualFileName));
 
   console.log("\nExecuting file ", fileFullPath);
-  helper.titleLog(actualFileName.replace(".js", "").replace(/_/g, " ").toUpperCase());
+  helper.titleLog(actualFileName.replace(".js", "").replace(/_/g, " ").toUpperCase() + " !!!");
 
   return fileFullPath;
+}
+
+function startPrompt() {
+  console.log("Enter zero(0) to exit the program");
+  console.log("Enter 'l' or 'list' to list the files\n");
+
+  prompt.get(["fileNumber"], function (err, result) {
+    if (err) console.error(err);
+    console.log("Input:", result);
+    const fileNumber = result.fileNumber;
+
+    if (["0", "zero"].some((r) => r == fileNumber.toLowerCase())) {
+      helper.warn("Program exited !!!");
+      prompt.stop();
+      process.exit(0);
+    }
+
+    if (["l", "list"].some((r) => r == fileNumber.toLowerCase())) {
+      logFiles();
+      startPrompt();
+      return;
+    }
+
+    if (!isNaN(Number(fileNumber)) && Number(fileNumber) > 0) {
+      const fileFullPath = getFile(fileNumber);
+
+      if (!fileFullPath) {
+        startPrompt();
+        return;
+      }
+
+      exec(`node ${fileFullPath}`, function (err, stdout, stderr) {
+        if (err) console.error(err);
+        if (stderr) console.error(stderr);
+        if (stdout) console.log(stdout);
+        console.log("-".repeat(50));
+        startPrompt();
+        return;
+      });
+    } else {
+      helper.error(`Invalid Input`);
+      startPrompt();
+      return;
+    }
+  });
 }
--- helper.js
+++ helper.js
@@ -1,7 +1,26 @@
+const LCERROR = "\x1b[31m%s\x1b[0m"; //red
+const LCWARN = "\x1b[33m%s\x1b[0m"; //yellow
+const LCINFO = "\x1b[36m%s\x1b[0m"; //cyan
+const LCSUCCESS = "\x1b[32m%s\x1b[0m"; //green
+
 exports.titleLog = function (...a) {
-  console.log(...a, "!!!\n");
+  console.log(LCSUCCESS, ...a, "\n");
+};
+
+exports.log = function (...a) {
+  console.log(LCINFO, ...a);
+};
+
+exports.success = function (...a) {
+  console.log(LCSUCCESS, ...a);
+};
+
+exports.warn = function (...a) {
+  console.warn(LCWARN, ...a);
 };
 
 exports.error = function (...a) {
-  console.error("ERROR:", ...a);
+  a.reverse().push("ERROR:");
+  a.reverse();
+  console.error(LCERROR, a.join(" "), "\n");
 };
